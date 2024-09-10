c0b61acb3a18679d3aff60343712a402fe26caf0
soojeong2 <dltnwud@naver.com>
2024-09-10 16:21:19 +0900

    openAIP Project v.1

---
+++ MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  MatzipApp
+//
+//  Created by 이수정 on 2024/09/10.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
